7810ca82d276038bd2eb8bbbdf2365428c00252e
Valeryia <valeria@paylogiq.com>
2025-10-27 00:27:15 +0100

    feat: implement read function from src/fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { join } from "path";
+import { readFile } from "fs/promises";
+
 const read = async () => {
   // Write your code here
+  const filePath = join(import.meta.dirname, "files", "fileToRead.txt");
+
+  try {
+    const content = await readFile(filePath, "utf-8");
+    console.log(content);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
