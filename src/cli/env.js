3f6353fdd22049efc095448aed640b8321ce97c0
Valeryia <valeria@paylogiq.com>
2025-10-27 00:44:41 +0100

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
   // Write your code here
+  const environmentVariables = Object.keys(process.env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join("; ");
+
+  console.log(environmentVariables);
 };
 
 parseEnv();
